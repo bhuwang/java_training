9cc872f2b009ee06cf56d457f9405b50cbed0ef5
Bhuwan Gautam <bhuwangautam@lftechnology.com>
2015-08-06 10:56:04 +0545

    #1 - Completed the hellow world salutation program.

---
+++ src/com/lftechnology/java/training/bhuwan/SalutaionDemo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.lftechnology.java.training.bhuwan;
+
+import java.util.Scanner;
+
+/**
+ * @author bhuwan
+ *
+ */
+public class SalutaionDemo {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your full name with salutation: ");
+        String fullNameWithSalutation = scanner.nextLine();
+        
+
+    }
+
+}
