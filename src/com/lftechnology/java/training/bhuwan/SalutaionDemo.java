b6e46a7bb487613735d8ede5c8f742cf450b9d2e
Bhuwan Gautam <bhuwangautam@lftechnology.com>
2015-08-10 10:35:17 +0545

    Test Commit

--- src/com/lftechnology/java/training/bhuwan/SalutaionDemo.java
+++ src/com/lftechnology/java/training/bhuwan/SalutaionDemo.java
@@ -6,8 +6,10 @@ package com.lftechnology.java.training.bhuwan;
 import java.util.Scanner;
 
 /**
+ * Create a Hello World Program which takes full name with salutation. Extract
+ * salutation and fullname from the argument and display.
+ * 
  * @author bhuwan
- *
  */
 public class SalutaionDemo {
 
@@ -18,7 +20,6 @@ public class SalutaionDemo {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your full name with salutation: ");
         String fullNameWithSalutation = scanner.nextLine();
-        
 
     }
 
