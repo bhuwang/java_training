445add73ec11cb26b2b0d1d47ae2686224a107fa
krishna <krishnatimilsina@lftechnology.com>
2015-08-12 14:25:45 +0545

    #8- format code

--- src/com/lftechnology/java/training/krishna/basics/smallest_multiple/SmallestMultipleDemo.java
+++ src/com/lftechnology/java/training/krishna/basics/smallest_multiple/SmallestMultipleDemo.java
@@ -6,7 +6,7 @@ public class SmallestMultipleDemo {
 	private final static Logger LOGGER = Logger.getLogger(SmallestMultiple.class.getName());
 
 	public static void main(String[] args) {
-        int number = 2520;
+		int number = 2520;
 		SmallestMultiple smallestMultiple = new SmallestMultiple();
 		long positiveNumber = smallestMultiple.smallestPositiveNumber(number);
 		LOGGER.info("smallest positive number : " + positiveNumber);
