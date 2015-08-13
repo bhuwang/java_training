0a073915828cfc3a4067ac27e8e239640db7b563
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-13 15:21:33 +0545

    #6 solve problem : find sum 0f event fibonacci number not exceed four million

---
+++ src/com/lftechnology/java/training/sanish/fibonacci/Fibonacci.java
@@ -0,0 +1,14 @@
+package com.lftechnology.java.training.sanish.fibonacci;
+
+import java.util.logging.Logger;
+/**
+ * Get Even Fibonacci numbers sum
+ * @author Sanish Maharjan <sanishmaharjan@lftechnology.com>
+ *
+ */
+public class Fibonacci {
+	private static final Logger LOGGER = Logger.getLogger(Fibonacci.class.getName());
+	public static void main(String args[]){
+		LOGGER.info("The sum of the even-valued terms of Fibonacci number not exceed four million : " + FibonacciNumber.sumEventFibonacciNumber(1, 2, 40));
+	}
+}
