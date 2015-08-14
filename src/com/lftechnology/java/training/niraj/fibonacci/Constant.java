69561d9dc9f88c0da838a8e8e00c719bb4e2295b
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-14 14:24:25 +0545

    #6 adds documentation for constants classs

--- src/com/lftechnology/java/training/niraj/fibonacci/Constant.java
+++ src/com/lftechnology/java/training/niraj/fibonacci/Constant.java
@@ -1,5 +1,11 @@
 package com.lftechnology.java.training.niraj.fibonacci;
 
+/**
+ * 
+ * Constants related to this application
+ * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+ *
+ */
 public class Constant {
 	public static final String ODD="odd";
 	public static final String EVEN="even";	
