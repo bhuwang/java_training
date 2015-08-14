288f42a722cbbf51eb783b140f91a8184f55a767
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-14 10:07:14 +0545

    #6 change max number and first number of fibonacci number

--- src/com/lftechnology/java/training/sanish/fibonacci/Fibonacci.java
+++ src/com/lftechnology/java/training/sanish/fibonacci/Fibonacci.java
@@ -9,9 +9,9 @@ import java.util.logging.Level;
  */
 public class Fibonacci {
 	private static final Logger LOGGER = Logger.getLogger(Fibonacci.class.getName());
-	private static final int FIRST_NUM = 1;
+	private static final int FIRST_NUM = 0;
 	private static final int SECOND_NUM = 1;
-	private static final int MAX_NUM = 400000;
+	private static final int MAX_NUM = 4000000;
 	public static void main(String args[]){
 		LOGGER.log(Level.INFO, "The sum of the even-valued terms of Fibonacci number not exceed four million : {0}", new Object[] {FibonacciNumber.sumEventFibonacciNumber(FIRST_NUM, SECOND_NUM, MAX_NUM) });
 	}
