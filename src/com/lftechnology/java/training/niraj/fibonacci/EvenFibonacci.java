b65a0dd36c152bf4b4075e766e1a8c63b6c3334c
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-14 11:27:04 +0545

    #6 adds funtionality to get sum of even numbers in a fibonacci series

---
+++ src/com/lftechnology/java/training/niraj/fibonacci/EvenFibonacci.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.lftechnology.java.training.niraj.fibonacci;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * find the sum of the even-valued terms in a fibonaci series under certain
+ * limit
+ *
+ */
+public class EvenFibonacci {
+
+	private static final Logger LOGGER = Logger.getLogger(EvenFibonacci.class
+			.getName());
+
+	public static void main(String[] args) {
+		Fibonacci fibonacciHelper = new Fibonacci();
+		int limit = 10;
+		int num1=1;
+		int num2=2;
+		int sum=fibonacciHelper.getSum(num1,num2,limit, "even");
+		LOGGER.log(Level.INFO, "The sum of even numbers in fibonacci series below {0} is {1}",
+				new Object[] { limit, sum });
+
+	}
+
+}
---
+++ src/com/lftechnology/java/training/niraj/fibonacci/Fibonacci.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.lftechnology.java.training.niraj.fibonacci;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import sun.security.util.BigInt;
+
+/**
+ * 
+ * Helper class related to fibonacci series
+ * 
+ * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+ *
+ */
+public class Fibonacci {
+
+
+	/**
+	 * Get Fibonacci series with certain limit
+	 * @param num1 {@link Integer}
+	 * @param num2 {@link Integer}
+	 * @param limit {@link Integer}
+	 * @return ArrayList containing numbers in a fibonacci Series
+	 * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+	 */
+	public ArrayList<Integer> getFibonacciSeries(int num1, int num2, int limit) {
+		ArrayList<Integer> series = new ArrayList<Integer>();
+		int sum = 0;
+		series.add(num1);
+		series.add(num2);
+		while (sum < limit) {
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+			series.add(sum);
+		}
+		return series;
+	}
+
+	/**
+	 * Get sum of odd/even number in a fibonacci series under certain limit
+	 * @param num1 {@link Integer}
+	 * @param num2 {@link Integer}
+	 * @param limit {@link Integer}
+	 * @param numType {@link String}
+	 * @return Sum of odd/even number in the series
+	 * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+	 */
+	public int getSum(int num1, int num2, int limit, String numType) {
+		int sum = 0;
+		int remainder = (numType == "odd") ? 1 : 0;
+		ArrayList<Integer> fibonacciSeries = getFibonacciSeries(num1, num2,
+				limit);
+		for (int num : fibonacciSeries) {
+			if (num % 2 == remainder) {
+				sum+=num;
+			}
+		}
+		return sum;
+	}
+
+}
