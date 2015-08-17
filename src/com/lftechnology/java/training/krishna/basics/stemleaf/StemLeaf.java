d131c89243664dc97de6fdeefcaffbd3e3022de5
krishna <krishnatimilsina@lftechnology.com>
2015-08-17 10:56:36 +0545

    #34 Java arrays: stem and leaf data presentation

---
+++ src/com/lftechnology/java/training/krishna/basics/stemleaf/StemLeaf.java
@@ -0,0 +1,99 @@
+package com.lftechnology.java.training.krishna.basics.stemleaf;
+
+/**
+ *  StemLeaf.java
+ *  
+ * This program calculates steam leaf and mode of values in an array of integers
+ * where the size of the array and elements of the array are provided as dataset
+ * by the user.
+ *
+ * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
+ * @version 1.0
+ */
+public class StemLeaf {
+	/**
+	 * This method computes the mean of the values in the dataset array.
+	 * 
+	 * @param dataset
+	 *            {@link array} array of dataset
+	 * @param datasetSize
+	 *            {@link int} size of dataset
+	 * @return mode - the mode of the dataset array
+	 */
+	public static int[][] getMode(int[] dataset, int datasetSize) {
+
+		dataset = bubbleSort(dataset, datasetSize);
+		int[][] stemLeaf = getStemLeaf(dataset, datasetSize);
+
+		// initialize 2D array storing numbers of occurrences, and values
+		int[][] mode = new int[datasetSize][2];
+		for (int i = 0; i < datasetSize; i++) {
+			for (int j = 0; j < 2; j++) {
+				mode[i][j] = 0;
+			}
+		}
+
+		// calculate mode
+		mode[0][0] = 1;
+		int count = 1;
+		for (int i = count - 1; i < datasetSize; i++) {
+			for (int j = count - 1; j < datasetSize - 1; j++) {
+				if (stemLeaf[i][0] == stemLeaf[j + 1][0]) {
+					count++;
+					mode[i][0]++;
+					mode[i][1] = stemLeaf[i][0];
+				} else if (i == 0)
+					mode[i][1] = stemLeaf[i][0];
+			}
+		}
+		return mode;
+
+	}
+
+	/**
+	 * This method returns a sorted version of the input array.
+	 * 
+	 * @param dataset
+	 *            {@link array} array of dataset
+	 * @param datasetSize
+	 *            {@link int} size of dataset
+	 * @return dataset - sorted version of the dataset array
+	 */
+	private static int[] bubbleSort(int[] dataset, int datasetSize) {
+
+		int temp = 0;
+
+		for (int i = 0; i < datasetSize; i++) {
+			for (int j = 1; j < (datasetSize - i); j++) {
+
+				if (dataset[j - 1] > dataset[j]) {
+					// swap the elements!
+					temp = dataset[j - 1];
+					dataset[j - 1] = dataset[j];
+					dataset[j] = temp;
+				}
+			}
+		}
+		return dataset;
+	}
+
+	/**
+	 * This program returns stem and leaf.
+	 * 
+	 * @param dataset
+	 *            {@link array} array of dataset
+	 * @param datasetSize
+	 *            {@link int} size of dataset
+	 * @return - steam and leaf in 2D array
+	 */
+	public static int[][] getStemLeaf(int[] dataset, int datasetSize) {
+
+		// store stem and leaf in a 2D array
+		int[][] stemLeaf = new int[datasetSize][2];
+		for (int i = 0; i < datasetSize; i++) {
+			stemLeaf[i][0] = dataset[i] / 10;
+			stemLeaf[i][1] = dataset[i] % 10;
+		}
+		return stemLeaf;
+	}
+}
---
+++ src/com/lftechnology/java/training/krishna/basics/stemleaf/StemLeafDemo.java
@@ -0,0 +1,91 @@
+package com.lftechnology.java.training.krishna.basics.stemleaf;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * StemLeafDemo.java
+ * 
+ * Log the output on the console.
+ * 
+ * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
+ * @version 1.0
+ */
+public class StemLeafDemo {
+	private final static Logger LOGGER = Logger.getLogger(StemLeafDemo.class.getName());
+
+	/**
+	 * This is the main method which makes use of displayStemLeafData() method.
+	 * 
+	 * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
+	 */
+	public static void main(String[] args) {
+		int n = 17;
+		// declare an array of n size to store integral data points
+		int dataset[] = new int[n];
+		dataset[0] = 44;
+		dataset[1] = 46;
+		dataset[2] = 47;
+		dataset[3] = 49;
+		dataset[4] = 63;
+		dataset[5] = 64;
+		dataset[6] = 66;
+		dataset[7] = 68;
+		dataset[8] = 68;
+		dataset[9] = 72;
+		dataset[10] = 72;
+		dataset[11] = 75;
+		dataset[12] = 76;
+		dataset[13] = 81;
+		dataset[14] = 84;
+		dataset[15] = 88;
+		dataset[16] = 106;
+		int datasetSize = dataset.length;
+
+		displayStemLeafData(dataset, datasetSize);
+	}
+
+	/**
+	 * This method display the steam and leaf on console.
+	 * 
+	 * @param dataset
+	 *            {@link array} array of dataset
+	 * @param datasetSize
+	 *            {@link int} size of dataset
+	 * 
+	 */
+	public static void displayStemLeafData(int[] dataset, int datasetSize) {
+
+		int[][] mode = StemLeaf.getMode(dataset, datasetSize);
+		int[][] stemLeaf = StemLeaf.getStemLeaf(dataset, datasetSize);
+
+		LOGGER.log(Level.INFO, "Stem and Leaf Display:");
+
+		int c = 0;
+		int leaf = 0;
+		for (int i = 0; i < datasetSize; i++) {
+			if (mode[i][1] != 0) {
+				leaf += mode[i][0];
+				List<Integer> numbers = new ArrayList<Integer>();
+				for (int j = c; j < leaf; j++) {
+					numbers.add(stemLeaf[j][1]);
+					c = leaf;
+				}
+				// StringBuilder to store comma separated string
+				StringBuilder commaSepLeaf = new StringBuilder();
+				// iterate over list,append numbers and comma until last element
+				for (int k = 0; k < numbers.size(); k++) {
+					commaSepLeaf.append(numbers.get(k));
+					if (k != numbers.size() - 1) {
+						commaSepLeaf.append(", ");
+					}
+				}
+				LOGGER.log(Level.INFO, "Frequency: {0} Steam: {1} Leaf: {2}",
+						new Object[] { mode[i][0], mode[i][1], commaSepLeaf });
+			}
+		}
+	}
+
+}
