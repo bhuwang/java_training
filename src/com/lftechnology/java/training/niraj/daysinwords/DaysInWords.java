0a6e21495ca4be1d810c96b30af4a6c10aadcbf6
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-17 11:08:54 +0545

    #9 adds functionality to get days in words

---
+++ src/com/lftechnology/java/training/niraj/daysinwords/DaysInWords.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.lftechnology.java.training.niraj.daysinwords;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * Get days in words
+ * 
+ * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+ *
+ */
+public class DaysInWords {
+
+	private static final Logger LOGGER = Logger.getLogger(DaysInWords.class
+			.getName());
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			LOGGER.log(Level.INFO, "Please provide a day in number (0-6) :");
+			while (!scanner.hasNextInt()) {
+				LOGGER.log(Level.INFO, "It's not a number. Please try again :");
+				scanner.next();
+			}
+			int day = scanner.nextInt();
+			String dayInWords = Days.getDayInWords(day);
+			LOGGER.log(Level.INFO, "The day you have selected is : {0}",
+					new Object[] { dayInWords });
+
+		} catch (InputMismatchException e) {
+			LOGGER.log(Level.WARNING, "Please provide a valid input.");
+		}
+
+	}
+
+}
