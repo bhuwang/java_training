cb739119b8e1ae60cb819f8febc2d611e9f15985
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-18 11:39:28 +0545

    #7 defines private constructor to avoid call to implicit public constructor of a class

--- src/com/lftechnology/java/training/niraj/palindrome/LargestPalindrome.java
+++ src/com/lftechnology/java/training/niraj/palindrome/LargestPalindrome.java
@@ -16,16 +16,17 @@ public class LargestPalindrome {
 
 	private static final Logger LOGGER = Logger
 			.getLogger(LargestPalindrome.class.getName());
+	
+	private LargestPalindrome(){
+		
+	}
 
 	public static void main(String[] args) {
 		int minLimit = 100;
 		int maxLimit = 999;
 		Palindrome palindromeHelper = new Palindrome(minLimit, maxLimit);
 		long largestPalindrome = palindromeHelper.getLargestPalindrome();
-		LOGGER.log(
-				Level.INFO,
-				"The largest palindrome for the product of two 3-digits number is {0}",
-				new Object[] { largestPalindrome });
+		LOGGER.log(Level.INFO,"The largest palindrome for the product of two 3-digits number is {0}",largestPalindrome);
 
 	}
 }
