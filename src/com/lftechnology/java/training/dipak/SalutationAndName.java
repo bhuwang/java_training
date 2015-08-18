be5c934cb80fa0ee6eb07030f71658cb6ca86208
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-18 15:35:33 +0545

    #1 fixes bugs

--- src/com/lftechnology/java/training/dipak/SalutationAndName.java
+++ src/com/lftechnology/java/training/dipak/SalutationAndName.java
@@ -8,25 +8,26 @@ import java.util.logging.Logger;
  * Extract salutation and fullname from the argument and display.
  */
 public class SalutationAndName {
-	private final static Logger LOGGER = Logger.getLogger(SalutationAndName.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(SalutationAndName.class.getName());
+
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		LOGGER.info("Enter your name with Salutation::");
-		String inputName=scanner.nextLine();
-		inputName=inputName.trim();
-		
-		String[] text=inputName.split(" ");
-		
-		LOGGER.info("Salutation::"+text[0]);
-		String name="";
-		
-		for(int i=1;i<text.length;i++){			
-			if(!(text[i].length()==0)){
-				name=name+text[i]+" ";
-			}			
-			
+		String inputName = scanner.nextLine();
+		inputName = inputName.trim();
+
+		String[] text = inputName.split(" ");
+
+		LOGGER.info("Salutation::" + text[0]);
+		String name = "";
+
+		for (int i = 1; i < text.length; i++) {
+			if (text[i].length() != 0) {
+				name = name + text[i] + " ";
+			}
+
 		}
-		LOGGER.info("Your name::"+name);
+		LOGGER.info("Your name::" + name);
 		scanner.close();
 	}
 
