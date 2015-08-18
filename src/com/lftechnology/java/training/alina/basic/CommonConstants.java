cff2c62f4e1b60ddd537df76c55ca8f3f0dcface
Alina <alinashakya@lftechnology.com>
2015-08-18 13:15:38 +0545

    dev - reordering of public and private variables

--- src/com/lftechnology/java/training/alina/basic/CommonConstants.java
+++ src/com/lftechnology/java/training/alina/basic/CommonConstants.java
@@ -7,11 +7,6 @@ package com.lftechnology.java.training.alina.basic;
  *
  */
 public class CommonConstants {
-
-	private CommonConstants() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static final String SUNDAY = "Sunday";
 	public static final String MONDAY = "Monday";
 	public static final String TUESDAY = "Tuesday";
@@ -20,4 +15,8 @@ public class CommonConstants {
 	public static final String FRIDAY = "Friday";
 	public static final String SATURDAY = "Saturday";
 	public static final String INVALID_DAY = "Not a valid day";
+	
+	private CommonConstants() {
+		// TODO Auto-generated constructor stub
+	}
 }
