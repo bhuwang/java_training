0859f166bc38a46b788090fac26bb9867b8be1e2
krishna <krishnatimilsina@lftechnology.com>
2015-08-18 12:57:42 +0545

    added documentation

--- src/com/lftechnology/java/training/krishna/basics/stemleaf/Utils.java
+++ src/com/lftechnology/java/training/krishna/basics/stemleaf/Utils.java
@@ -2,14 +2,32 @@ package com.lftechnology.java.training.krishna.basics.stemleaf;
 
 import java.util.List;
 
+/**
+ * Utils.java
+ * 
+ * This program works for String Utilities.
+ * 
+ * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
+ * @version 1.0
+ */
 public class Utils {
 	
+	/**
+	 * This method implode the input array with separater.
+	 * 
+	 * @param array
+	 *            {@link array} input array
+	 * @param separator
+	 *            {@link String} separator like "-"
+	 *            
+	 * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
+	 */
 	public static StringBuilder implode(List<Integer> array, String separator){
 
 		int arraySize = array.size();
-		// StringBuilder to store comma separated string
+		// StringBuilder to store separator separated string
 		StringBuilder output = new StringBuilder();
-		// iterate over list,append numbers and comma until last element
+		// iterate over list,append numbers and separator until last element
 		for (int k = 0; k < arraySize; k++) {
 			output.append(array.get(k));
 			if (k != arraySize - 1) {
