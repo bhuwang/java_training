ef3cb8b7d6bcfb6baf0a059e2ffdc254c9326319
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-18 11:43:51 +0545

    #8 defines private constructor to avoid call to implicit public constructor of a class

--- src/com/lftechnology/java/training/niraj/smallestmultiple/SmallestMultiple.java
+++ src/com/lftechnology/java/training/niraj/smallestmultiple/SmallestMultiple.java
@@ -18,15 +18,16 @@ public class SmallestMultiple {
 	private static Logger LOGGER = Logger.getLogger(SmallestMultiple.class
 			.getName());
 
+	private SmallestMultiple() {
+
+	}
+
 	public static void main(String[] args) {
 		int minNum = 1;
 		int maxNum = 20;
 		Multiples multiplesHelper = new Multiples(minNum, maxNum);
 		int smallestMultiple = multiplesHelper.smallestMultiple();
-		LOGGER.log(
-				Level.INFO,
-				"The smallest positive number that is evenly divisible by all of the numbers from {0} to {1} is {2}",
-				new Object[] { minNum, maxNum, smallestMultiple });
+		LOGGER.log(Level.INFO,"The smallest positive number that is evenly divisible by all of the numbers from {0} to {1} is {2}",new Object[] { minNum, maxNum, smallestMultiple });
 
 	}
 }
