e4076868764cdda9d45b33143de3fd7331c38adf
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-18 12:00:31 +0545

    Array designators placc on the type

--- src/com/lftechnology/java/training/sanish/multiplenumber/MultipleNumber.java
+++ src/com/lftechnology/java/training/sanish/multiplenumber/MultipleNumber.java
@@ -13,7 +13,7 @@ public class MultipleNumber {
 	private static final int FROM_NUM = 1;
 	private static final int TO_NUM = 20;
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		try {
 			LOGGER.log(Level.INFO, "The smallest positive number that is divisible by all of the numbers from {0} to {1} : {2}", new Object[] {FROM_NUM, TO_NUM, Multiplier.getSmallerMultipleNum(FROM_NUM, TO_NUM)});
 		} catch (ArithmeticException e) {
