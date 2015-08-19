ecec3bcbbbea04e228ea8cc4ced891de2a367fb7
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-19 13:35:21 +0545

    #33 adds the functionality to get complete league information for a particular team

---
+++ src/com/lftechnology/java/training/niraj/leaguetable/Constants.java
@@ -0,0 +1,19 @@
+package com.lftechnology.java.training.niraj.leaguetable;
+
+/**
+ * 
+ * Constants related to league table package
+ * 
+ * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+ *
+ */
+public class Constants {
+
+	public static final String SCORED = "scored";
+	public static final String CONCEDED = "conceded";
+
+	private Constants() {
+
+	}
+
+}
