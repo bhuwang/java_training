777f26244efce7cfba8b8635596e73f96a5800c2
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-19 09:31:24 +0545

    #1 fixes bugs

--- src/com/lftechnology/java/training/dipak/Test.java
+++ src/com/lftechnology/java/training/dipak/Test.java
@@ -11,7 +11,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter your name with Salutation::");
+		LOGGER.info("Enter your name with Salutation::");
 		String inputName = scanner.nextLine();
 		inputName = inputName.trim();
 
@@ -21,7 +21,7 @@ public class Test {
 		String name = "";
 
 		for (int i = 1; i < text.length; i++) {
-			if (!(text[i].length() == 0)) {
+			if (text[i].length() != 0) {
 				name = name + text[i] + " ";
 			}
 
