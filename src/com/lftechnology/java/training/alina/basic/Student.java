29cb15a894c887a6bc42755d18858d6f7f5a83d4
Alina <alinashakya@lftechnology.com>
2015-08-19 20:16:31 +0545

    #34 - removed unused parenthesis & used override method

--- src/com/lftechnology/java/training/alina/basic/Student.java
+++ src/com/lftechnology/java/training/alina/basic/Student.java
@@ -17,7 +17,8 @@ public class Student extends Course implements Comparable<Course> {
 	static ArrayList<Student> students = new ArrayList<Student>();
 	static ArrayList<Course> courses = new ArrayList<Course>();
 
-	private static final Logger LOGGER = Logger.getLogger(Student.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(Student.class
+			.getName());
 	private String fullName;
 	private int standard;
 	private int rollNo;
@@ -70,8 +71,10 @@ public class Student extends Course implements Comparable<Course> {
 			LOGGER.log(Level.INFO, "Enter Student's Full Name : ");
 			String fullName = input.nextLine();
 			MarksValidation validation = new MarksValidation();
-			int standard = validation.numberValidation(input, "Enter Standard : ");
-			int rollNumber = validation.numberValidation(input, "Enter Roll Number : ");
+			int standard = validation.numberValidation(input,
+					"Enter Standard : ");
+			int rollNumber = validation.numberValidation(input,
+					"Enter Roll Number : ");
 			LOGGER.log(Level.INFO, "Enter Marks for following Subjects : ");
 			float maths = validation.marksValidation(input, "Maths : ");
 			float science = validation.marksValidation(input, "Science : ");
@@ -82,7 +85,7 @@ public class Student extends Course implements Comparable<Course> {
 					history, fullName, standard, rollNumber);
 			students.add(student);
 		}
-		displayStudentDetails(students, courses);
+		displayStudentDetails(students);
 	}
 
 	/**
@@ -90,16 +93,14 @@ public class Student extends Course implements Comparable<Course> {
 	 * 
 	 * @param students
 	 *            {@link ArrayList} arrays of student details
-	 * @param courses
-	 *            {@link ArrayList} arrays of course details
 	 * @author Alina Shakya <alinashakya@lftechnology.com>
 	 */
-	private static void displayStudentDetails(ArrayList<Student> students,
-			ArrayList<Course> courses) {
+	private static void displayStudentDetails(ArrayList<Student> students) {
 		Comparator<Student> mycomparator = Collections.reverseOrder();
 		Collections.sort(students, mycomparator);
 		for (Student student : students) {
-			LOGGER.log(Level.INFO, "Student lists : {0}", new Object[] { student });
+			LOGGER.log(Level.INFO, "Student lists : {0}",
+					new Object[] { student });
 		}
 	}
 
@@ -108,6 +109,7 @@ public class Student extends Course implements Comparable<Course> {
 				+ ", Roll Number : " + this.rollNo + "," + super.toString();
 	}
 
+	@Override
 	public int compareTo(Course val) {
 		int percentageToCompare = (int) val.getPercentage();
 		return (int) (this.getPercentage() - percentageToCompare);
