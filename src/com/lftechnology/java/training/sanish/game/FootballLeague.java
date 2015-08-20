9fb6394c710f1b07c548f226bc4870d32c61391b
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-20 21:43:04 +0545

    football league result functionality completed

---
+++ src/com/lftechnology/java/training/sanish/game/FootballLeague.java
@@ -0,0 +1,23 @@
+package com.lftechnology.java.training.sanish.game;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class FootballLeague {
+    private final static Logger LOGGER = Logger.getLogger(FootballLeague.class.getName());
+    private static String resultSet = "Manchester United 1 Chelsea 0, Arsenal 1 Manchester United 1, Manchester United 3 Fulham 1, Liverpool 2 Manchester United 1, Swansea 2 Manchester United 4";
+    private static ArrayList<Game> games = new ArrayList<Game>();
+
+    public static void main(String[] args) {
+        Game game;
+        String teamName = "Manchester United";
+        String[] gameResultSets = resultSet.split(", ");
+        for (String gameResultText : gameResultSets) {
+            game = new Game(gameResultText);
+            game.processGameResult();
+            games.add(game);
+        }
+
+        LeagueResult.displayLeagueGameResultOfTeam(games, teamName);
+    }
+}
---
+++ src/com/lftechnology/java/training/sanish/game/LeagueResult.java
@@ -0,0 +1,43 @@
+package com.lftechnology.java.training.sanish.game;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LeagueResult {
+    private static final Logger LOGGER = Logger.getLogger(LeagueResult.class.getName());
+
+    private LeagueResult() {
+    }
+
+    public static void displayLeagueGameResultOfTeam(ArrayList<Game> games, String teamName) {
+        Team team;
+        int totalGoalScore = 0, totalGoalConceded = 0, totalGameScore = 0, winGameCount = 0,
+                lossGameCount = 0, drawGameCount = 0;
+        for (Game game : games) {
+            team = game.getTeamByName(teamName);
+            if (team != null) {
+                totalGoalScore += team.getGoalScored();
+                totalGoalConceded += team.getGoalConceded();
+                totalGameScore += team.getGamePoints();
+                if (team.getGameResult() == Constants.WIN) {
+                    winGameCount++;
+                } else if (team.getGameResult() == Constants.LOSS) {
+                    lossGameCount++;
+                } else {
+                    drawGameCount++;
+                }
+            }
+        }
+
+        LOGGER.log(Level.INFO, "League game result \n" +
+                "==================================== \n" +
+                "No. of wins \t {0} \n" +
+                "No. of draws \t {1} \n" +
+                "No. of defeats \t {2} \n" +
+                "Goal Scored \t {3} \n" +
+                "Goal Conceded \t {4} \n" +
+                "No. of points \t {5}", new Object[]{winGameCount, drawGameCount, lossGameCount, totalGoalScore,
+                totalGoalConceded, totalGameScore});
+    }
+}
