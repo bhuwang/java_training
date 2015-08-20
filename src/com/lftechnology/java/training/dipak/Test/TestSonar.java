ba905982b69c20193b9d702908427261ff1a3708
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-20 17:57:35 +0545

    First commit

---
+++ src/com/lftechnology/java/training/dipak/Test/TestSonar.java
@@ -0,0 +1,13 @@
+package com.lftechnology.java.training.dipak.Test;
+
+import java.util.logging.Logger;
+
+public class TestSonar {
+	private final static Logger LOGGER = Logger.getLogger(TestSonar.class.getName());
+	int a[]=new int[-1];
+	
+	public static void main(String argument[] ) {
+		LOGGER.info("This is a test program",1);
+	}
+
+}
