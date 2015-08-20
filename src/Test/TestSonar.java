e3be03a591f151eedf436ceb5ce27406f6779354
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-20 17:51:32 +0545

    testing sonar

---
+++ src/Test/TestSonar.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.logging.Logger;
+
+public class TestSonar {
+	private final static Logger LOGGER = Logger.getLogger(TestSonar.class.getName());
+	int a[]=new int[2];
+	
+	public static void main(String argument[] ) {
+		LOGGER.info("This is a test program");
+	}
+
+}
