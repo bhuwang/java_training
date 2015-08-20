6f3841f32c475dece373deaf37af6fbff266b1b4
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-20 09:28:07 +0545

    #6 removes complex looping statement

--- src/com/lftechnology/java/training/dipak/fibonacci/EvenFibonacci.java
+++ src/com/lftechnology/java/training/dipak/fibonacci/EvenFibonacci.java
@@ -62,7 +62,8 @@ public class EvenFibonacci {
 		int sum = 0;
 		for (int i = 0; i < index; i++) {
 			if (checkEven(series[i])) {
-				sum += series[i];
+				sum += series[i];	
+				
 			}
 		}
 		return sum;
