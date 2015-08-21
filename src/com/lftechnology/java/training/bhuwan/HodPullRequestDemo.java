da6430db78b7e1ff4e09f3a5dfbb9b279b3bf20f
Bhuwan Gautam <bhuwangautam@lftechnology.com>
2015-08-21 17:33:53 +0545

    Pull Request Demo

---
+++ src/com/lftechnology/java/training/bhuwan/HodPullRequestDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.lftechnology.java.training.bhuwan;
+
+import java.util.Random;
+
+/**
+ * @author bhuwan
+ */
+public class HodPullRequestDemo {
+
+    public double add(double numberA, double numberB) {
+        int abc;
+        System.out.println("we should not use system.out");
+        return new Double(0.0);
+    }
+
+}
