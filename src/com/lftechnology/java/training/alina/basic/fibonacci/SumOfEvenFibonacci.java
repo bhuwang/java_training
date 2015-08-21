deb723262ce78c6a218eb2f7fe6a15c1dac1b132
Alina <alinashakya@lftechnology.com>
2015-08-21 13:07:20 +0545

    dev - created package for fibonacci

--- src/com/lftechnology/java/training/alina/basic/fibonacci/SumOfEvenFibonacci.java
+++ src/com/lftechnology/java/training/alina/basic/fibonacci/SumOfEvenFibonacci.java
@@ -1,4 +1,4 @@
-package com.lftechnology.java.training.alina.basic;
+package com.lftechnology.java.training.alina.basic.fibonacci;
 
 /**
  * The SumOfEvenFibonacci program implements an application that simply
