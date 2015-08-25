845df2771035040465ff741840d57cf8cef3ea72
Alina <alinashakya@lftechnology.com>
2015-08-25 17:42:23 +0545

    #84 - adds package level documentation

--- src/com/lftechnology/java/training/alina/basic/packagejarexecute/package-info.java
+++ src/com/lftechnology/java/training/alina/basic/packagejarexecute/package-info.java
@@ -1,9 +1,26 @@
 /**
+ * <p>This package consists of two functionalities, print number(NumberPrint Class) 
+ * and add two numbers(NumberAdd Class).</p>
+ * <ul>
+ * <li>NumberPrint Class : </li>
+ * <ol type="a">
+ * <li>Create a Java application PrintNumber which prints number from 1-10.</li>
+ * <li>Create Executable Jar</li>
+ * <li>Run java application from command line.</li>
+ * <li>Print numbers</li>
+ * <li>Create JavaDoc of Application</li>
+ * </ol>
+ * <li>NumberAdd Class : </li>
+ * <ol type="a">
+ * <li>Create a Java application AddNumber, which takes two number from User and prints sum.</li>
+ * <li>Create Executable Jar</li>
+ * <li>Run java application from command line which should take values from user.</li>
+ * <li>Print sum</li>
+ * <li>Create JavaDoc of Application</li>
+ * </ol>
+ * </ul>
  * 
- */
-/**
  * @author Alina Shakya <alinashakya@lftechnology.com>
- *
  */
 
 package com.lftechnology.java.training.alina.basic.packagejarexecute;
