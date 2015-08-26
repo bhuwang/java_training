1a3b80968068db406806b7aee955f40fb5e88a61
dipakthapaofficial <dipakthapaofficial@gmail.com>
2015-08-26 12:50:27 +0545

    #11 computes gpa and displays students in sorted order

---
+++ src/com/lftechnology/java/training/dipak/gpa/GpaCalculator.java
@@ -0,0 +1,26 @@
+package com.lftechnology.java.training.dipak.gpa;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class GpaCalculator {
+	private GpaCalculator(){
+		
+	}
+	/**
+	 * This method computes the gpa.
+	 * @param marks
+	 * @return sum
+	 */
+	public static float calculateGpa(int[] marks) {
+		float sum=0;
+		for(int i=0;i<marks.length;i++){
+			sum+=marks[i];
+		}
+		sum=(sum/500)*100;
+		sum=new BigDecimal(sum/25).setScale(1, RoundingMode.HALF_UP).floatValue();
+		System.out.println(sum);
+		return sum;
+	}
+
+}
---
+++ src/com/lftechnology/java/training/dipak/gpa/Results.java
@@ -0,0 +1,17 @@
+package com.lftechnology.java.training.dipak.gpa;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Results {
+	private static final Logger LOGGER = Logger.getLogger(Results.class.getName());
+	/**
+	 * This method displays the students along with their name and respective GPAs.
+	 * @param gp
+	 */
+	public static void displayResults(GPA[] gp){
+		for (int i = 0; i < gp.length; i++) {
+			LOGGER.log(Level.INFO,"Name:{0} 	Roll:{1} 	class:{2}	 GPA:{3}",new Object[]{gp[i].studentName,gp[i].studentRoll,gp[i].studentClass,gp[i].studentGpa});
+		}
+	}
+}
