f0d70809bad4974a84d2db7ec930102891cff815
krishna <krishnatimilsina@lftechnology.com>
2015-08-26 16:10:23 +0545

    implemented interface

--- src/com/lftechnology/java/training/krishna/basics/gpa/Subject.java
+++ src/com/lftechnology/java/training/krishna/basics/gpa/Subject.java
@@ -1,42 +1,47 @@
+
 package com.lftechnology.java.training.krishna.basics.gpa;
 
 /**
- * Subject.java
+ * Subject.java This program store subject and marks information
  * 
- * This program store subject and marks information
- * 
- *
  * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
  * @version 1.0
  */
 public class Subject {
-	private String subject;
+
+	private String subjectName;
 	private int marks;
 
-	public Subject(String subject, int marks) {
-		this.subject = subject;
+	public Subject(String subjectName, int marks) {
+
+		this.subjectName = subjectName;
 		this.marks = marks;
 	}
 
-	public String getSubject() {
-		return subject;
+	public String getSubjectName() {
+
+		return subjectName;
 	}
 
-	public void setSubject(String subject) {
-		this.subject = subject;
+	public void setSubjectName(String subjectName) {
+
+		this.subjectName = subjectName;
 	}
 
 	public int getMarks() {
+
 		return marks;
 	}
 
 	public void setMarks(int marks) {
+
 		this.marks = marks;
 	}
 
 	@Override
 	public String toString() {
-		return "Subject [subject=" + subject + ", marks=" + marks + "]";
+
+		return "Subject [subjectName=" + subjectName + ", marks=" + marks + "]";
 	}
 
 }
