23077ba49044f333f6189b80ce342d28632745ca
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-26 14:43:50 +0545

    #84 jar file created

---
+++ src/com/lftechnology/java/training/sanish/numberprinter/NumberPrinter.java
@@ -0,0 +1,32 @@
+package com.lftechnology.java.training.sanish.numberprinter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Print number for 1-10
+ *
+ * @author Sanish Maharjan <sanishmaharjan@lftechnology.com>
+ */
+public class NumberPrinter {
+    private static final Logger LOGGER = Logger.getLogger(NumberPrinter.class.getName());
+
+    public static void main(String[] agrs) {
+        System.out.print("Hello world.");
+    }
+
+    /**
+     * Print number from min to max parameter provided
+     * @param min {@link Integer} minimum number of number series
+     * @param max {@link Integer}  maximum number of number series
+     * @author Sanish Maharjan <sanishmaharjan@lftechnology.com>
+     */
+    public static void printNumber(int min, int max) {
+        String numberSeries = "" + min;
+        for (int i = min + 1; i <= max; i++) {
+            numberSeries += ", " + i;
+        }
+
+       LOGGER.log(Level.INFO, "Number from 1-10 : {0}", new Object[]{numberSeries});
+    }
+}
