773030dee3bb05c9c755b3064781b88e8d1276e9
Alina <alinashakya@lftechnology.com>
2015-08-31 14:07:31 +0545

    #84 - modified jar file of number add and its doc file

--- src/com/lftechnology/java/training/alina/basic/packagejarexecute/NumberAdd.java
+++ src/com/lftechnology/java/training/alina/basic/packagejarexecute/NumberAdd.java
@@ -1,21 +1,15 @@
 
 package com.lftechnology.java.training.alina.basic.packagejarexecute;
 
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * <p>NumberAdd Class is used to display the sum of two numbers</p>
- * 
- * <p>Steps : </p>
- * <ol type="a">
- * <li>Create a Java application AddNumber, which takes two number from User and prints sum.</li>
- * <li>Create Executable Jar</li>
- * <li>Run java application from command line which should take values from user.</li>
- * <li>Print sum</li>
- * <li>Create JavaDoc of Application</li>
- * </ol>
+ * <p>NumberAdd Class is used to display the sum of two numbers</p> <p>Steps :
+ * </p> <ol type="a"> <li>Create a Java application AddNumber, which takes two
+ * number from User and prints sum.</li> <li>Create Executable Jar</li> <li>Run
+ * java application from command line which should take values from user.</li>
+ * <li>Print sum</li> <li>Create JavaDoc of Application</li> </ol>
  * 
  * @author Alina Shakya <alinashakya@lftechnology.com>
  */
@@ -25,44 +19,17 @@ public class NumberAdd {
 		Logger.getLogger(NumberAdd.class.getName());
 
 	/**
-	 * Main method used to calculate and display the sum of two numbers
+	 * Main method used to calculate and display the sum of numbers
 	 * 
 	 * @param args
 	 * @author Alina Shakya <alinashakya@lftechnology.com>
 	 */
 	public static void main(String[] args) {
 
-		NumberValidation numValidation = new NumberValidation();
-		try (Scanner input = new Scanner(System.in)) {
-			float firstNumber =
-				numValidation.numberValidation(input, "Enter first number : ");
-			float secondNumber =
-				numValidation.numberValidation(input, "Enter second number : ");
-			float sumOfTwoNumbers =
-				calculateSumOfNumbers(firstNumber, secondNumber);
-			LOGGER.log(
-				Level.INFO, "Sum of two numbers is : {0}", sumOfTwoNumbers);
+		int totalSum = 0;
+		for (int i = 0; i < args.length; i++) {
+			totalSum += Integer.parseInt(args[i]);
 		}
-		catch (Exception e) {
-			LOGGER.log(Level.WARNING, "Number Invalid Exception.");
-		}
-	}
-
-	/**
-	 * Function used to calculate sum of two numbers
-	 * 
-	 * @param firstNumber
-	 *            {@link Float}
-	 * @param secondNumber
-	 *            {@link Float}
-	 * @return totalSum {@link Float} Sum of two numbers
-	 * @author Alina Shakya <alinashakya@lftechnology.com>
-	 */
-	private static float calculateSumOfNumbers(
-		float firstNumber, float secondNumber) {
-
-		float totalSum;
-		totalSum = firstNumber + secondNumber;
-		return totalSum;
+		LOGGER.log(Level.INFO, "Sum of two numbers is : {0}", totalSum);
 	}
 }
