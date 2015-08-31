65e5333baa4f513c9ebe29d3a62ea2d0b09d04c2
Alina <alinashakya@lftechnology.com>
2015-08-31 15:44:02 +0545

    dev - adds java formatter

--- src/com/lftechnology/java/training/alina/basic/calculategpa/Student.java
+++ src/com/lftechnology/java/training/alina/basic/calculategpa/Student.java
@@ -1,4 +1,3 @@
-
 package com.lftechnology.java.training.alina.basic.calculategpa;
 
 import java.util.ArrayList;
@@ -15,107 +14,98 @@ import java.util.logging.Logger;
  */
 public class Student extends Course {
 
-	static ArrayList<Student> students = new ArrayList<Student>();
-	static ArrayList<Course> courses = new ArrayList<Course>();
+    static ArrayList<Student> students = new ArrayList<Student>();
+    static ArrayList<Course> courses = new ArrayList<Course>();
 
-	private static final Logger LOGGER =
-		Logger.getLogger(Student.class.getName());
-	private String fullName;
-	private int standard;
-	private int rollNo;
+    private static final Logger LOGGER = Logger.getLogger(Student.class.getName());
+    private String fullName;
+    private int standard;
+    private int rollNo;
 
-	public Student(String fullName, int standard, int rollNo) {
+    public Student(String fullName, int standard, int rollNo) {
 
-		this.fullName = fullName;
-		this.standard = standard;
-		this.rollNo = rollNo;
-	}
+        this.fullName = fullName;
+        this.standard = standard;
+        this.rollNo = rollNo;
+    }
 
-	public void setFullName(String fullName) {
+    public void setFullName(String fullName) {
 
-		this.fullName = fullName;
-	}
+        this.fullName = fullName;
+    }
 
-	public String getFullName() {
+    public String getFullName() {
 
-		return fullName;
-	}
+        return fullName;
+    }
 
-	public void setStandard(int standard) {
+    public void setStandard(int standard) {
 
-		this.standard = standard;
-	}
+        this.standard = standard;
+    }
 
-	public int getStandard() {
+    public int getStandard() {
 
-		return standard;
-	}
+        return standard;
+    }
 
-	public void setRollNo(int rollNo) {
+    public void setRollNo(int rollNo) {
 
-		this.rollNo = rollNo;
-	}
+        this.rollNo = rollNo;
+    }
 
-	public int getRollNo() {
+    public int getRollNo() {
 
-		return rollNo;
-	}
+        return rollNo;
+    }
 
-	/**
-	 * Function used to get all the details regarding students performance
-	 * Displays students details with their marks
-	 * 
-	 * @param input
-	 *            {@link Scanner}
-	 * @param studentNo
-	 *            {@link Integer} number of students
-	 * @author Alina Shakya <alinashakya@lftechnology.com>
-	 */
-	public static void getStudentDetails(Scanner input, int studentNo) {
+    /**
+     * Function used to get all the details regarding students performance Displays students details with their marks
+     * 
+     * @param input
+     *            {@link Scanner}
+     * @param studentNo
+     *            {@link Integer} number of students
+     * @author Alina Shakya <alinashakya@lftechnology.com>
+     */
+    public static void getStudentDetails(Scanner input, int studentNo) {
 
-		for (int i = 0; i < studentNo; i++) {
-			LOGGER.log(Level.INFO, "Enter Student's Full Name : ");
-			String fullName = input.nextLine();
-			MarksValidation validation = new MarksValidation();
-			int standard =
-				validation.numberValidation(input, "Enter Standard : ");
-			int rollNumber =
-				validation.numberValidation(input, "Enter Roll Number : ");
-			LOGGER.log(Level.INFO, "Enter Marks for following Subjects : ");
-			float maths = validation.marksValidation(input, "Maths : ");
-			float science = validation.marksValidation(input, "Science : ");
-			float english = validation.marksValidation(input, "English : ");
-			float nepali = validation.marksValidation(input, "Nepali : ");
-			float history = validation.marksValidation(input, "History : ");
-			Student student = new Student(fullName, standard, rollNumber);
-			Course course =
-				new Course(maths, science, english, nepali, history);
-			students.add(student);
-			courses.add(course);
-		}
-		displayStudentDetails(students, courses);
-	}
+        for (int i = 0; i < studentNo; i++) {
+            LOGGER.log(Level.INFO, "Enter Student's Full Name : ");
+            String fullName = input.nextLine();
+            MarksValidation validation = new MarksValidation();
+            int standard = validation.numberValidation(input, "Enter Standard : ");
+            int rollNumber = validation.numberValidation(input, "Enter Roll Number : ");
+            LOGGER.log(Level.INFO, "Enter Marks for following Subjects : ");
+            float maths = validation.marksValidation(input, "Maths : ");
+            float science = validation.marksValidation(input, "Science : ");
+            float english = validation.marksValidation(input, "English : ");
+            float nepali = validation.marksValidation(input, "Nepali : ");
+            float history = validation.marksValidation(input, "History : ");
+            Student student = new Student(fullName, standard, rollNumber);
+            Course course = new Course(maths, science, english, nepali, history);
+            students.add(student);
+            courses.add(course);
+        }
+        displayStudentDetails(students, courses);
+    }
 
-	/**
-	 * Function used to display all the student performance details
-	 * 
-	 * @param students
-	 *            {@link ArrayList} arrays of student details
-	 * @author Alina Shakya <alinashakya@lftechnology.com>
-	 */
-	private static void displayStudentDetails(
-		ArrayList<Student> students, ArrayList<Course> courses) {
+    /**
+     * Function used to display all the student performance details
+     * 
+     * @param students
+     *            {@link ArrayList} arrays of student details
+     * @author Alina Shakya <alinashakya@lftechnology.com>
+     */
+    private static void displayStudentDetails(ArrayList<Student> students, ArrayList<Course> courses) {
 
-		Comparator<Student> mycomparator = Collections.reverseOrder();
-		Collections.sort(students, mycomparator);
-		LOGGER.log(Level.INFO, "Student lists : {0} , {1}", new Object[] {
-			students.toString(), courses.toString()
-		});
-	}
+        Comparator<Student> mycomparator = Collections.reverseOrder();
+        Collections.sort(students, mycomparator);
+        LOGGER.log(Level.INFO, "Student lists : {0} , {1}", new Object[] { students.toString(), courses.toString() });
+    }
 
-	public String toString() {
+    public String toString() {
 
-		return "Name : " + this.fullName + ", Class : " + this.standard +
-			", Roll Number : " + this.rollNo;
-	}
+        return "Name : " + this.fullName + ", Class : " + this.standard + ", Roll Number : " + this.rollNo;
+    }
 }
