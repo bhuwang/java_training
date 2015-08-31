7fc900dc30c765375dc43c667450a133ac477518
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-08-31 15:34:58 +0545

    #84 handles numberformat exception

--- src/com/lftechnology/java/training/niraj/addnumber/AddNumber.java
+++ src/com/lftechnology/java/training/niraj/addnumber/AddNumber.java
@@ -1,4 +1,3 @@
-
 package com.lftechnology.java.training.niraj.addnumber;
 
 import java.util.InputMismatchException;
@@ -13,33 +12,39 @@ import java.util.logging.Logger;
  */
 public class AddNumber {
 
-	private static final Logger LOGGER =
-		Logger.getLogger(AddNumber.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(AddNumber.class.getName());
 
-	private AddNumber() {
+    private AddNumber() {
 
-	}
+    }
 
-	public static void main(String[] args) {		
-			int num1 = Integer.parseInt(args[0]);
-			int num2 = Integer.parseInt(args[1]);
-			int sum = AddNumber.add(num1, num2);
-			LOGGER.log(Level.INFO, "Sum:{0}", sum);		
-	}
+    public static void main(String[] args) {
+        try {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+            int sum = AddNumber.add(num1, num2);
+            LOGGER.log(Level.INFO, "Sum:{0}", sum);
+        } catch (NumberFormatException e) {
+            LOGGER.log(Level.SEVERE, "Exception:{0}", e);
+        }
 
-	/**
-	 * <p>Gets the sum of two number</p>
-	 * 
-	 * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
-	 * @param num1
-	 *            int
-	 * @param num2
-	 *            int
-	 * @return sum of two numbers
-	 */
-	public static int add(int num1, int num2) {
+    }
 
-		return num1 + num2;
-	}
+    /**
+     * <p>
+     * Gets the sum of two number
+     * </p>
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param num1
+     *            int
+     * @param num2
+     *            int
+     * @return sum of two numbers
+     */
+    public static int add(int num1, int num2) {
+
+        return num1 + num2;
+    }
 
 }
