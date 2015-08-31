e378cd3f325f1fda592b664171fb7ba880a7bcd6
Alina <alinashakya@lftechnology.com>
2015-08-31 13:07:45 +0545

    #87 - removed unused import utils

--- src/com/lftechnology/java/training/alina/collections/generics/wildcards/CustomerUtil.java
+++ src/com/lftechnology/java/training/alina/collections/generics/wildcards/CustomerUtil.java
@@ -1,9 +1,6 @@
 
 package com.lftechnology.java.training.alina.collections.generics.wildcards;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * CustomerUtil Class consists of comparisions of different customer
  * 
