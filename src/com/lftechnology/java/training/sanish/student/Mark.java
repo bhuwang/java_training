6427775bafb5495d4d13d5fec79f907e498c44a3
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-08-31 11:49:45 +0545

    code format

--- src/com/lftechnology/java/training/sanish/student/Mark.java
+++ src/com/lftechnology/java/training/sanish/student/Mark.java
@@ -2,37 +2,37 @@ package com.lftechnology.java.training.sanish.student;
 
 /**
  * Provide mark information
- * 
+ *
  * @author Sanish Maharjan <sanishmaharjan@lftechnology.com>
  */
 public class Mark {
-	private String subject;
-	private int markObtain;
+    private String subject;
+    private int markObtain;
 
-	public Mark(String subject, int markObtain) {
-		this.subject = subject;
-		this.markObtain = markObtain;
-	}
+    public Mark(String subject, int markObtain) {
+        this.subject = subject;
+        this.markObtain = markObtain;
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	public int getMarkObtain() {
-		return markObtain;
-	}
+    public int getMarkObtain() {
+        return markObtain;
+    }
 
-	public void setMarkObtain(int markObtain) {
-		this.markObtain = markObtain;
-	}
+    public void setMarkObtain(int markObtain) {
+        this.markObtain = markObtain;
+    }
 
-	@Override
-	public String toString() {
-		return "Mark [subject=" + subject + ", markObtain=" + markObtain + "]";
-	}
+    @Override
+    public String toString() {
+        return "Mark [subject=" + subject + ", markObtain=" + markObtain + "]";
+    }
 
 }
