19cf39589905d97163be38af6bc9b3c9c4fcb892
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2015-09-03 17:25:01 +0545

    general formating mistakes solved

--- src/com/lftechnology/java/training/srijan/basics/genericstutorial/GenericSumCalculator.java
+++ src/com/lftechnology/java/training/srijan/basics/genericstutorial/GenericSumCalculator.java
@@ -12,74 +12,87 @@ public class GenericSumCalculator {
 	private static final Logger LOGGER = Logger.getLogger(GenericSumCalculator.class.getName());
 	List<Integer> intList = new ArrayList<Integer>();
 	List<Double> doubleList = new ArrayList<Double>();
-	
-	public GenericSumCalculator(){
+
+	public GenericSumCalculator() {
 		this.numberOfInputs = 0;
 	}
-	
+
 	/**
-	 * <p>inputs number of inputs user will give</p>
-	 * <p>inputs integer numbers</p>
+	 * <p>
+	 * inputs number of inputs user will give
+	 * </p>
+	 * <p>
+	 * inputs integer numbers
+	 * </p>
+	 * 
 	 * @author srijan
 	 */
-	public void inputIntegerData(){
+	public void inputIntegerData() {
 		Scanner input = null;
-		try{
-		input = new Scanner(System.in);
-		int inputNumber = 0;
-		LOGGER.info("Enter number of integer you will input");
-		numberOfInputs = input.nextInt();
-		LOGGER.info("Enter integer numbers");
-		for(int i =0;i<numberOfInputs;i++){
-			inputNumber = input.nextInt();
-			intList.add(inputNumber);
-		}
-		LOGGER.log(Level.INFO,"numbers entered in integer list:{0}",intList);
-		addNumbers(intList);
-		}catch(InputMismatchException ex){
+		try {
+			input = new Scanner(System.in);
+			int inputNumber = 0;
+			LOGGER.info("Enter number of integer you will input");
+			numberOfInputs = input.nextInt();
+			LOGGER.info("Enter integer numbers");
+			for (int i = 0; i < numberOfInputs; i++) {
+				inputNumber = input.nextInt();
+				intList.add(inputNumber);
+			}
+			LOGGER.log(Level.INFO, "numbers entered in integer list:{0}", intList);
+			addNumbers(intList);
+		} catch (InputMismatchException ex) {
 			LOGGER.info("please enter integer value:" + ex);
-		}finally{
-		input.close();
-		}
-	}
-	
-	/**
-	 * <p>inputs from user  number of inputs user will give</p>
-	 * <p>inputs double numbers</p>
-	 * @author srijan
-	 */
-	public void inputDoubleData(){
-		Scanner input = null;
-		try{
-		input = new Scanner(System.in);
-		
-		double inputNumber = 0;
-		LOGGER.info("Enter number of double number you will input");
-		numberOfInputs = input.nextInt();
-		LOGGER.info("Enter numbers(double type");
-		for(int i = 0;i<numberOfInputs;i++){
-			inputNumber = input.nextDouble();
-			doubleList.add(inputNumber);
-		}
-		LOGGER.log(Level.INFO,"numbers entered in double list: {0}",doubleList);
-		addNumbers(doubleList);
-		}catch(InputMismatchException ex){
-			LOGGER.info("please enter double value" + ex);
-		}finally{
+		} finally {
 			input.close();
 		}
-		
 	}
-	
+
 	/**
-	 * <p>adds integer and double from a list and shows result</p>
+	 * <p>
+	 * inputs from user number of inputs user will give
+	 * </p>
+	 * <p>
+	 * inputs double numbers
+	 * </p>
+	 * 
+	 * @author srijan
+	 */
+	public void inputDoubleData() {
+		Scanner input = null;
+		try {
+			input = new Scanner(System.in);
+
+			double inputNumber = 0;
+			LOGGER.info("Enter number of double number you will input");
+			numberOfInputs = input.nextInt();
+			LOGGER.info("Enter numbers(double type");
+			for (int i = 0; i < numberOfInputs; i++) {
+				inputNumber = input.nextDouble();
+				doubleList.add(inputNumber);
+			}
+			LOGGER.log(Level.INFO, "numbers entered in double list: {0}", doubleList);
+			addNumbers(doubleList);
+		} catch (InputMismatchException ex) {
+			LOGGER.info("please enter double value" + ex);
+		} finally {
+			input.close();
+		}
+
+	}
+
+	/**
+	 * <p>
+	 * adds integer and double from a list and shows result
+	 * </p>
+	 * 
 	 * @param list
 	 */
-	public void addNumbers(List<? extends Number> list){
+	public void addNumbers(List<? extends Number> list) {
 		double sum = 0;
-		for(Number number : list){
+		for (Number number : list) {
 			sum += number.doubleValue();
 		}
-		LOGGER.log(Level.INFO,"The sum: {0}", sum);
+		LOGGER.log(Level.INFO, "The sum: {0}", sum);
 	}
 }
