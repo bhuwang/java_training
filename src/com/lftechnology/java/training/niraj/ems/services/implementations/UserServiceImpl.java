1688685e15a2de759b3e85bb27564a5ec5a3b538
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-09-14 16:43:49 +0545

    #32 adds services related to Employee and User domains

--- src/com/lftechnology/java/training/niraj/ems/services/implementations/UserServiceImpl.java
+++ src/com/lftechnology/java/training/niraj/ems/services/implementations/UserServiceImpl.java
@@ -1,22 +1,34 @@
 package com.lftechnology.java.training.niraj.ems.services.implementations;
 
+import java.io.Console;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 import com.lftechnology.java.training.niraj.ems.domains.Employee;
+import com.lftechnology.java.training.niraj.ems.enums.Roles;
+import com.lftechnology.java.training.niraj.ems.enums.Status;
 import com.lftechnology.java.training.niraj.ems.exceptions.CustomException;
 import com.lftechnology.java.training.niraj.ems.services.CrudService;
-import com.lftechnology.java.training.niraj.ems.services.Roles;
-import com.lftechnology.java.training.niraj.ems.services.Status;
+import com.lftechnology.java.training.niraj.ems.utils.Constants;
+import com.lftechnology.java.training.niraj.ems.utils.UserNumInputImpl;
+import com.lftechnology.java.training.niraj.ems.utils.UserStringInputImpl;
+import com.lftechnology.java.training.niraj.ems.utils.Utils;
 
 public class UserServiceImpl implements CrudService<Employee, String> {
 
+    private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class.getName());
     private EmployeeDaoImpl employeeDao;
+    private UserStringInputImpl userInput;
+    private UserNumInputImpl userNumInput;
 
     public UserServiceImpl() {
         employeeDao = new EmployeeDaoImpl();
+        userInput = new UserStringInputImpl();
+        userNumInput = new UserNumInputImpl();
     }
 
     @Override
@@ -26,7 +38,6 @@ public class UserServiceImpl implements CrudService<Employee, String> {
 
     @Override
     public Employee find(String s) throws SQLException {
-        // TODO Auto-generated method stub
         return employeeDao.find(s);
     }
 
@@ -36,22 +47,52 @@ public class UserServiceImpl implements CrudService<Employee, String> {
 
     }
 
-    public boolean login(String username, String password) throws SQLException {
+    /**
+     * Logs a user in
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param username
+     * @param password
+     * @return
+     * @throws SQLException
+     */
+    public Employee login(String username, String password) throws SQLException {
         Map<String, String> credentials = new LinkedHashMap<String, String>();
         credentials.put("username", username);
         credentials.put("password", password);
-        return employeeDao.exists(credentials);
+        credentials.put("status", Status.ACTIVE.getStatus());
+        credentials.put("isTerminated", Status.INACTIVE.getStatus());
+        Employee employee = employeeDao.findByAttributes(credentials);
+
+        return employee;
     }
 
-    public boolean saveUserInfo(Map<String, String> userInfo) throws SQLException {
+    /**
+     * Saves user information
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param userInfo
+     * @return
+     * @throws SQLException
+     */
+    public String saveUserInfo(Map<String, String> userInfo) throws SQLException {
         Employee employee = new Employee();
         parseUserInfo(employee.getAttributes(), userInfo);
-        setEmployeeInfo(employee, userInfo);
+        employeeDao.setEmployeeObject(userInfo, employee);
         save(employee);
-        return employee.getId() != null ? true : false;
+        return employee.getId() != null ? employee.getId() : null;
 
     }
 
+    /**
+     * Terminates an employee
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param id
+     * @return
+     * @throws SQLException
+     * @throws CustomException
+     */
     public boolean terminateEmployee(String id) throws SQLException, CustomException {
         Employee employee = employeeDao.find(id);
         boolean isTerminated = false;
@@ -64,6 +105,14 @@ public class UserServiceImpl implements CrudService<Employee, String> {
         return isTerminated;
     }
 
+    /**
+     * Deletes user
+     * 
+     * @param id
+     * @return
+     * @throws SQLException
+     * @throws CustomException
+     */
     public boolean delete(String id) throws SQLException, CustomException {
         Employee employee = employeeDao.find(id);
         boolean isDeleted = false;
@@ -76,39 +125,364 @@ public class UserServiceImpl implements CrudService<Employee, String> {
         return isDeleted;
     }
 
+    /**
+     * Gets user registration informations
+     * 
+     * @author Niraj Rajbhandari<nirajrajbhandari@lftechnology.com>
+     * 
+     * @param scanner
+     * @param console
+     * @return
+     */
+    public Map<String, String> getUserRegistrationInfo(Scanner scanner, Console console) {
+        Map<String, String> userRegistrationInfo = new LinkedHashMap<String, String>();
+        userRegistrationInfo.put("fullname", userInput.getInput(scanner, console, Constants.FULLNAME_LABEL));
+        userRegistrationInfo.put("username", userInput.getInput(scanner, console, Constants.USERNAME_LABEL));
+        userRegistrationInfo.put("password", userInput.getInput(scanner, console, Constants.PASSWORD_LABEL));
+        userRegistrationInfo.put("address", userInput.getInput(scanner, console, Constants.ADDRESS_LABEL));
+        LOGGER.info(Constants.DEPARTMENT_LABEL);
+        int department = userNumInput.getInput(scanner, 1, 8);
+        userRegistrationInfo.put("department", getDepartmentById(department));
+        LOGGER.info(Constants.ROLE_LABEL);
+        int role = userNumInput.getInput(scanner, 1, 2);
+        if (role == 1) {
+            userRegistrationInfo.put("role", Roles.ADMIN.getRole());
+        } else {
+            userRegistrationInfo.put("role", Roles.USER.getRole());
+        }
+
+        return userRegistrationInfo;
+    }
+
+    /**
+     * Gets user credentials
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @return
+     */
+    public Map<String, String> getUserCredentials(Scanner scanner, Console console) {
+        Map<String, String> credentials = new LinkedHashMap<String, String>();
+        credentials.put("username", userInput.getInput(scanner, console, Constants.USERNAME_LABEL));
+        credentials.put("password", userInput.getInput(scanner, console, Constants.PASSWORD_LABEL));
+        return credentials;
+    }
+
+    /**
+     * Registers a user
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param employeeInfo
+     * @return
+     * @throws SQLException
+     */
+    public Map<String, String> registerUser(Map<String, String> employeeInfo) throws SQLException {
+        String userId = saveUserInfo(employeeInfo);
+        Map<String, String> employeeAfterRegistration = null;
+        if (userId != null) {
+            Employee employee = find(userId);
+            employeeAfterRegistration = employee.getInfo();
+        }
+        return employeeAfterRegistration;
+
+    }
+
+    /**
+     * Adds a user
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @return
+     * @throws SQLException
+     * @throws CustomException
+     */
+    public Employee addUser(Scanner scanner, Console console) throws SQLException, CustomException {
+        Employee employee = new Employee();
+        Map<String, String> userRegistrationInfo = getUserRegistrationInfo(scanner, console);
+        userRegistrationInfo = registerUser(userRegistrationInfo);
+
+        if (!Utils.isEmpty(userRegistrationInfo.get("username"))) {
+            employee = setAttributes(employee, userRegistrationInfo);
+        } else {
+            throw new CustomException("Failed to add user. Please try again.");
+        }
+        return employee;
+    }
+
+    /**
+     * Get department by id
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param id
+     * @return
+     */
+    private String getDepartmentById(int id) {
+        String department;
+        switch (id) {
+        case 2:
+            department = "ROR";
+            break;
+        case 3:
+            department = "PHP";
+            break;
+        case 4:
+            department = "Devops";
+            break;
+        case 5:
+            department = "Android";
+            break;
+        case 6:
+            department = "iOS";
+            break;
+        case 7:
+            department = "Administration";
+            break;
+        case 8:
+            department = "Operations";
+            break;
+        default:
+            department = "Java";
+            break;
+        }
+        return department;
+    }
+
+    /**
+     * Parses user information
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param attributes
+     * @param userInfo
+     */
     private void parseUserInfo(List<String> attributes, Map<String, String> userInfo) {
         for (String attribute : attributes) {
             if (!userInfo.containsKey(attribute)) {
                 userInfo.put(attribute, null);
             }
         }
+        if (userInfo.get("status") == null) {
+            userInfo.put("status", Status.ACTIVE.getStatus());
+        }
+        if (userInfo.get("isTerminated") == null) {
+            userInfo.put("isTerminated", Status.INACTIVE.getStatus());
+        }
     }
 
-    private void setEmployeeInfo(Employee employee, Map<String, String> userInfo) {
-        employee.setAddress(userInfo.get("address"));
-        employee.setDepartment(userInfo.get("department"));
-        employee.setFullname(userInfo.get("fullname"));
-        employee.setPassword(userInfo.get("password"));
-        String role = userInfo.get("role");
-        if (role.equals(Roles.ADMIN.getRole())) {
-            employee.setRole(Roles.ADMIN);
-        } else {
-            employee.setRole(Roles.USER);
-        }
-        String isTerminated = userInfo.get("isTerminated");
-        if (isTerminated.equals(Status.ACTIVE.getStatus())) {
-            employee.setIsTerminated(Status.ACTIVE);
-        } else {
-            employee.setIsTerminated(Status.INACTIVE);
-        }
+    /**
+     * Sets attributes
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * 
+     * @param employee
+     * @param userInfo
+     * @return
+     */
+    public Employee setAttributes(Employee employee, Map<String, String> userInfo) {
+        employeeDao.setEmployeeObject(userInfo, employee);
+        return employee;
 
-        String status = userInfo.get("status");
-        if (status.equals(Status.INACTIVE.getStatus())) {
-            employee.setStatus(Status.INACTIVE);
-        } else {
-            employee.setStatus(Status.ACTIVE);
-        }
-
-        employee.setUsername(userInfo.get("username"));
     }
+
+    /**
+     * Searches user
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @return
+     * @throws SQLException
+     */
+    public String searchUser(Scanner scanner, Console console) throws SQLException {
+        Map<String, String> searchCondition = new LinkedHashMap<String, String>();
+        searchCondition.put("username", userInput.getInput(scanner, console, Constants.SEARCH_BY_USERNAME));
+        searchCondition.put("status", Status.ACTIVE.getStatus());
+        List<Employee> searchResults = employeeDao.findAll(searchCondition);
+        if (searchResults.size() == 0) {
+            return Constants.USER_NOT_FOUND;
+        }
+        return getFormattedSearchResults(searchResults);
+    }
+
+    /**
+     * Edits user profile
+     * 
+     * @author Niraj Rajbhandari<nirajrajbhandari@lftechnology.com>
+     * @param employee
+     * @param scanner
+     * @param console
+     * @throws SQLException
+     */
+    public void editUserProfile(Employee employee, Scanner scanner, Console console) throws SQLException {
+        List<String> choices = userInput.getMultipleChoices(scanner, console, Constants.EDIT_OPTION_LABEL, 1, 4);
+        Map<String, String> editInfo = getEditInfo(scanner, console, choices);
+        Employee editedEmployee = setAttributes(employee, editInfo);
+        save(editedEmployee);
+    }
+
+    /**
+     * Gets users list
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @return
+     * @throws SQLException
+     */
+    public String getUsersList() throws SQLException {
+        Map<String, String> searchCondition = new LinkedHashMap<String, String>();
+        searchCondition.put("status", Status.ACTIVE.getStatus());
+        List<Employee> searchResults = employeeDao.findAll(searchCondition);
+        if (searchResults.size() == 0) {
+            return Constants.USER_NOT_FOUND;
+        }
+        return getFormattedSearchResults(searchResults);
+    }
+
+    /**
+     * Get total users in the db
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @return
+     * @throws SQLException
+     */
+    public int count() throws SQLException {
+        return employeeDao.getCount();
+    }
+
+    /**
+     * Terminates or deletes user
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @throws SQLException
+     */
+    public void TerminateDeleteUser(Scanner scanner, Console console) throws SQLException, CustomException {
+        Map<String, String> condition = new LinkedHashMap<String, String>();
+        condition.put("username", userInput.getInput(scanner, console, Constants.SELECT_USER_EDIT));
+        Employee employee = employeeDao.findByAttributes(condition);
+        if (employee.getId() == null) {
+            LOGGER.warning(Constants.USER_NOT_FOUND);
+        } else {
+            editSelectedUser(scanner, console, employee);
+        }
+
+    }
+
+    /**
+     * Gets the users choice to terminate or delete a user
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @param employee
+     * @throws SQLException
+     * @throws CustomException
+     */
+    private void editSelectedUser(Scanner scanner, Console console, Employee employee) throws SQLException, CustomException {
+        LOGGER.info(Constants.DELETE_TERMINATE_MENU);
+        int selectedAction = userNumInput.getInput(scanner, 1, 2);
+        boolean completed;
+        switch (selectedAction) {
+        case 1:
+            completed = terminateEmployee(employee.getId());
+            if (completed) {
+                LOGGER.info(Constants.SUCCESS_TERMINATE_USER);
+            } else {
+                LOGGER.severe(Constants.FAIL_TERMINATE_USER);
+            }
+            break;
+        case 2:
+            completed = delete(employee.getId());
+            if (completed) {
+                LOGGER.info(Constants.SUCCESS_DELETE_USER);
+            } else {
+                LOGGER.severe(Constants.FAIL_DELETE_USER);
+            }
+            break;
+
+        default:
+            LOGGER.severe(Constants.INVALID_INPUT);
+            break;
+        }
+    }
+
+    /**
+     * Gets edit information
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param scanner
+     * @param console
+     * @param choices
+     * @return
+     */
+    private Map<String, String> getEditInfo(Scanner scanner, Console console, List<String> choices) {
+        Map<String, String> editInfo = new LinkedHashMap<String, String>();
+        for (String choice : choices) {
+            switch (choice) {
+            case "2":
+                editInfo.put("address", userInput.getInput(scanner, console, Constants.ADDRESS_LABEL));
+                break;
+            case "3":
+                LOGGER.info(Constants.DEPARTMENT_LABEL);
+                int department = userNumInput.getInput(scanner, 1, 8);
+                editInfo.put("department", getDepartmentById(department));
+                break;
+            case "4":
+                editInfo.put("password", userInput.getInput(scanner, console, Constants.PASSWORD_LABEL));
+                break;
+            default:
+                editInfo.put("fullname", userInput.getInput(scanner, console, Constants.FULLNAME_LABEL));
+                break;
+            }
+        }
+        return editInfo;
+    }
+
+    /**
+     * Formats the search result
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param searchResults
+     * @return
+     */
+    private String getFormattedSearchResults(List<Employee> searchResults) {
+        StringBuilder formattedSearchResult = new StringBuilder();
+        formattedSearchResult.append(Constants.SEARCH_RESULT_HEADER);
+        formattedSearchResult.append(Constants.DIVIDER);
+        for (Employee employee : searchResults) {
+            formattedSearchResult.append("\n");
+            formattedSearchResult.append(employee.getFullname());
+            formattedSearchResult.append("\t\t");
+            formattedSearchResult.append(employee.getUsername());
+            formattedSearchResult.append("\t\t");
+            formattedSearchResult.append(employee.getAddress());
+            formattedSearchResult.append("\t\t   ");
+            formattedSearchResult.append(employee.getDepartment());
+            formattedSearchResult.append("\t\t\t");
+            formattedSearchResult.append(employee.getRole());
+            formattedSearchResult.append("\t\t");
+            formattedSearchResult.append(getStatus(employee.getIsTerminated()));
+            formattedSearchResult.append("\n");
+        }
+        return formattedSearchResult.toString();
+    }
+
+    /**
+     * Gets status string
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param status
+     * @return
+     */
+    private String getStatus(String status) {
+        String statusString;
+        if (status.equals(Status.ACTIVE.getStatus())) {
+            statusString = Status.ACTIVE.getStatusString();
+        } else {
+            statusString = Status.INACTIVE.getStatusString();
+        }
+        return statusString;
+    }
+
 }
