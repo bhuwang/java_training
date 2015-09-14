c615e3c428f0f3cd6478990ea8a5be700f8531e3
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-09-14 16:41:57 +0545

    #32 adds entry point for ems

--- src/com/lftechnology/java/training/niraj/ems/EmployeeManagement.java
+++ src/com/lftechnology/java/training/niraj/ems/EmployeeManagement.java
@@ -1,5 +1,8 @@
 package com.lftechnology.java.training.niraj.ems;
 
+
+import com.lftechnology.java.training.niraj.ems.services.implementations.RouteServices;
+
 /**
  * Employee Management System
  * 
@@ -7,8 +10,21 @@ package com.lftechnology.java.training.niraj.ems;
  *
  */
 public class EmployeeManagement {
+
+    private EmployeeManagement() {
+
+    }
+
     public static void main(String[] args) {
-        
+        EmployeeManagement mgmt = new EmployeeManagement();
+        mgmt.index();
+    }
+
+    /**
+     * Entry point to the management system
+     */
+    private void index() {
+        RouteServices.routeLandingPage();
     }
 
 }
