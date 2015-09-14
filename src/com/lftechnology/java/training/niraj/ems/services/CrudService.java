ac307f77c9a2b5ef51458dd94049175085b12f03
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2015-09-14 16:43:06 +0545

    #32 adds interface for CRUD operations

--- src/com/lftechnology/java/training/niraj/ems/services/CrudService.java
+++ src/com/lftechnology/java/training/niraj/ems/services/CrudService.java
@@ -1,11 +1,34 @@
 package com.lftechnology.java.training.niraj.ems.services;
 
-
 public interface CrudService<T, S> {
 
+    /**
+     * Saves information
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param t
+     * @return
+     * @throws Exception
+     */
     public T save(T t) throws Exception;
 
+    /**
+     * Finds a record
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param s
+     * @return
+     * @throws Exception
+     */
     public T find(S s) throws Exception;
 
+    /**
+     * Updates a record
+     * 
+     * @author Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
+     * @param t
+     * @return
+     * @throws Exception
+     */
     public boolean update(T t) throws Exception;
 }
