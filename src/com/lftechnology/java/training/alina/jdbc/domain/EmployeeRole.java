23c0504a3e9faa2913c166101bc3a0ff7b6c6aff
Alina <alinashakya@lftechnology.com>
2015-09-15 13:26:06 +0545

    #32 - adds documentations

--- src/com/lftechnology/java/training/alina/jdbc/domain/EmployeeRole.java
+++ src/com/lftechnology/java/training/alina/jdbc/domain/EmployeeRole.java
@@ -1,5 +1,10 @@
 package com.lftechnology.java.training.alina.jdbc.domain;
 
+/**
+ * EmployeeRole Class defines roles of employee
+ * 
+ * @author Alina Shakya <alinashakya@lftechnology.com>
+ */
 public enum EmployeeRole {
     ADMIN("admin"), USER("user");
     public String role;
