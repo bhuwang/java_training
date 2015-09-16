f4b99ac740771aef1f330309b3917f4cfcc2a501
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-09-16 09:26:32 +0545

    #32 add private constructor in main class

--- src/com/lftechnology/java/training/sanish/application/App.java
+++ src/com/lftechnology/java/training/sanish/application/App.java
@@ -3,6 +3,9 @@ package com.lftechnology.java.training.sanish.application;
 import com.lftechnology.java.training.sanish.application.controller.LoginController;
 
 public class App {
+    private App() {
+    }
+
     public static void main(String[] args) {
         LoginController.LoginPage();
     }
