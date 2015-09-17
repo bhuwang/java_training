c40f97e8a3416faea686da27234a5c68feca5aa3
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2015-09-17 11:58:48 +0545

    #32 change StringBuffer to StringBuilder

--- src/com/lftechnology/java/training/sanish/application/component/UserLogin.java
+++ src/com/lftechnology/java/training/sanish/application/component/UserLogin.java
@@ -99,7 +99,7 @@ public class UserLogin {
         try {
             java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
             byte[] array = md.digest(str.getBytes());
-            StringBuffer sb = new StringBuffer();
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < array.length; ++i) {
                 sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
             }
